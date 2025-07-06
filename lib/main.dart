e91aca190cdb8250dcc980eb6c1e46ab0b4bbbfd
saiprashanth751 <saiprashanth751@gmail.com>
2025-07-06 23:10:24 +0530

    habit-track

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,118 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 void main() {
-  runApp(App());
+  runApp(HabitTrackerApp());
 }
 
-class App extends StatelessWidget {
+class HabitTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoadingScreen(),
+      home: HabitTrackerHome(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class LoadingScreen extends StatefulWidget {
-  @override
-  State<LoadingScreen> createState() => _LoadingScreenState();
+class Habit {
+  final String title;
+  bool isDone;
+  double progress;
+
+  Habit({required this.title, this.isDone = false, this.progress = 0});
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
-  bool isDone = false;
-
+class HabitTrackerHome extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3), () {
-      setState(() {
-        isDone = true;
-      });
+  State<HabitTrackerHome> createState() => _HabitTrackerHomeState();
+}
+
+class _HabitTrackerHomeState extends State<HabitTrackerHome> {
+  List<Habit> habits = [
+    Habit(title: 'Drink Water'),
+    Habit(title: 'Exercise'),
+    Habit(title: 'Read Book'),
+    Habit(title: 'Meditate'),
+  ];
+
+  void toggleHabit(int index) {
+    setState(() {
+      var habit = habits[index];
+      habit.isDone = !habit.isDone;
+      habit.progress = habit.isDone ? 1.0 : 0.0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return isDone ? OnboardingScreen() : Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
-  }
-}
-
-class OnboardingScreen extends StatefulWidget {
-  @override
-  State<OnboardingScreen> createState() => _OnboardingScreenState();
-}
-
-class _OnboardingScreenState extends State<OnboardingScreen> {
-  double opacity1 = 0;
-  double opacity2 = 0;
-  double opacity3 = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(milliseconds: 500), () => setState(() => opacity1 = 1));
-    Timer(Duration(milliseconds: 1000), () => setState(() => opacity2 = 1));
-    Timer(Duration(milliseconds: 1500), () => setState(() => opacity3 = 1));
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[50],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedOpacity(
-              opacity: opacity1,
-              duration: Duration(milliseconds: 800),
-              child: Text(
-                'Welcome to Flutter App',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      appBar: AppBar(title: Text('Habit Tracker')),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: habits.length,
+        itemBuilder: (context, index) {
+          final habit = habits[index];
+          return Card(
+            margin: EdgeInsets.only(bottom: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            elevation: 4,
+            child: InkWell(
+              onTap: () => toggleHabit(index),
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            habit.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        AnimatedSwitcher(
+                          duration: Duration(milliseconds: 300),
+                          transitionBuilder: (child, anim) =>
+                              ScaleTransition(scale: anim, child: child),
+                          child: habit.isDone
+                              ? Icon(Icons.check_circle, color: Colors.green, key: ValueKey(true))
+                              : Icon(Icons.radio_button_unchecked, color: Colors.grey, key: ValueKey(false)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 500),
+                      height: 10,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                          width: MediaQuery.of(context).size.width * habit.progress * 0.8,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 20),
-            AnimatedOpacity(
-              opacity: opacity2,
-              duration: Duration(milliseconds: 800),
-              child: Text(
-                'Build beautiful UIs',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            SizedBox(height: 20),
-            AnimatedOpacity(
-              opacity: opacity3,
-              duration: Duration(milliseconds: 800),
-              child: Text(
-                'Fast development, expressive design',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
