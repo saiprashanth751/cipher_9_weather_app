d6420dcb88d13dad3d89adf12dedbe96f789b894
saiprashanth751 <saiprashanth751@gmail.com>
2025-07-06 22:15:00 +0530

    calc app

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(CalculatorApp());
 }
 
-class MyApp extends StatelessWidget {
+class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Productivity App',
-      home: StartScreen(),
+      home: CalculatorPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class StartScreen extends StatefulWidget {
+class CalculatorPage extends StatefulWidget {
   @override
-  _StartScreenState createState() => _StartScreenState();
+  State<CalculatorPage> createState() => _CalculatorPageState();
 }
 
-class _StartScreenState extends State<StartScreen> {
-  int counter = 0;
+class _CalculatorPageState extends State<CalculatorPage> {
+  TextEditingController num1Controller = TextEditingController();
+  TextEditingController num2Controller = TextEditingController();
+  double result = 0;
 
-  void incrementCounter() {
-    setState(() {
-      counter++;
-    });
+  void calculate(String op) {
+    double n1 = double.tryParse(num1Controller.text) ?? 0;
+    double n2 = double.tryParse(num2Controller.text) ?? 0;
+
+    switch (op) {
+      case '+':
+        result = n1 + n2;
+        break;
+      case '-':
+        result = n1 - n2;
+        break;
+      case '×':
+        result = n1 * n2;
+        break;
+      case '÷':
+        result = n2 != 0 ? n1 / n2 : 0;
+        break;
+    }
+
+    setState(() {});
+  }
+
+  Widget buildButton(String op) {
+    return ElevatedButton(
+      onPressed: () => calculate(op),
+      child: Text(op),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Productivity App'),
-      ),
+      appBar: AppBar(title: Text('Simple Calculator')),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Hi, I am Sai Prashanth!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            TextField(
+              controller: num1Controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'First Number'),
             ),
-            SizedBox(height: 10),
-            Text(
-              '"Stay focused. Stay productive!"',
-              style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+            TextField(
+              controller: num2Controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Second Number'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: ['+', '-', '×', '÷'].map((op) => buildButton(op)).toList(),
             ),
             SizedBox(height: 30),
             Text(
-              'Counter value:',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              '$counter',
-              style: TextStyle(fontSize: 40, color: Colors.blue),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: incrementCounter,
-              child: Text('Increment'),
+              'Result: $result',
+              style: TextStyle(fontSize: 24),
             ),
           ],
         ),
